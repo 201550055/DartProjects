7db37d637fbc55511dcbdba43cb92d5e24097146
201550055 <91643503+201550055@users.noreply.github.com>
2022-07-02 14:55:52 +0530

    Add files via upload

---
+++ 17-ExpressionFunction.dart
@@ -0,0 +1,18 @@
+/*Expression function are functions with no name
+
+ */
+
+main(){
+  print(sum(10,15));
+}
+
+/*
+int sum(int i,int j){
+  return i+j;
+}
+*/
+
+//function expression
+int sum(i,j) => i+j;
+//=> arrow operator
+// no need of return statement
