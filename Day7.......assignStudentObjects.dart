df746dd82bb0589d534054ed50352f800d28cb53
201550055 <91643503+201550055@users.noreply.github.com>
2022-07-02 15:03:08 +0530

    Add files via upload

---
+++ Day7.......assignStudentObjects.dart
@@ -0,0 +1,9 @@
+
+import 'Day7.......assignStudentClass.dart';
+void main(){
+  var obj1=new student("Bob","B","A++",80);
+  obj1.NameOfStudent();
+  obj1.SectionOfStudent();
+  obj1.GradeOfStudent();
+  obj1.AttendanceOfStudent();
+}
