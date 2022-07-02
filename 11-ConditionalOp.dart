2e435292a202c3df844cef45e1f208ea5d502c56
201550055 <91643503+201550055@users.noreply.github.com>
2022-07-02 14:53:50 +0530

    Add files via upload

---
+++ 11-ConditionalOp.dart
@@ -0,0 +1,10 @@
+void main() {
+  String greeting = "hii";
+
+  String msg = (greeting == "hi") ? "Welcome" : "Bye";
+
+  print(msg);
+}
+
+/*
+ condition ? statement1(executed when condition is true) : statement 2(executed when condition is false)*/
