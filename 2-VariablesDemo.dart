3e186589c75fbd230636203ec65c9b41337a1cf8
201550055 <91643503+201550055@users.noreply.github.com>
2022-07-02 14:48:44 +0530

    Create 2-VariablesDemo.dart

---
+++ 2-VariablesDemo.dart
@@ -0,0 +1,24 @@
+// Single Line Comment
+
+/*  .................
+Multi Line Comment
+ */
+
+// cntrl + / (Shortcut for comment and uncomment)
+
+// variable is a container that holds the information
+
+void main()
+{
+  var x;   //the variable is loosely typed(can store different types of data)
+  x="Pankaj";
+  x=30;
+  x=true;
+  var a = 10;
+  var b = 20;
+  var c = a+b;
+  print(c);
+  print("Result= $c" );    //interpolation  ,it means message as well as data......concatenation of string + variable
+}
+
+//var is keyword for declaring variables and they are loosely typed
